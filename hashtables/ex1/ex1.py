0185ff450ae609662c4565fb7d2fc15996d25285
Kronykus <MoreheadSteve@gmail.com>
2020-09-11 22:02:13 -0500

    done

--- hashtables/ex1/ex1.py
+++ hashtables/ex1/ex1.py
@@ -3,5 +3,14 @@ def get_indices_of_item_weights(weights, length, limit):
     YOUR CODE HERE
     """
     # Your code here
+    cache = {}
 
+    for i, weight in enumerate( weights ):
+        # check cache for value, return if found
+        if limit - weight in cache:
+            return ( i, cache[ limit - weight ] )
+        # wasn't in cache
+        cache[ weight ] = i
+
+    # doesn't exist
     return None
--- hashtables/ex2/ex2.py
+++ hashtables/ex2/ex2.py
@@ -10,5 +10,21 @@ def reconstruct_trip(tickets, length):
     YOUR CODE HERE
     """
     # Your code here
+    cache = {}
+    route = [ None ] * length
+
+    # cache tickets
+    for ticket in tickets:
+        cache[ ticket.source ] = ticket.destination
+
+    #print( cache )
+    
+    # NONE for first
+    cur = cache[ 'NONE' ]
+
+    # loop through with each dest as next source
+    for i in range( length ):
+        route[ i ] = cur
+        cur = cache[ cur ]
 
     return route
